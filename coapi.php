8a206a7873a7e32f61121422a98a88f26b08c0c1
FolkXone <72967134+FolkXone@users.noreply.github.com>
2021-12-07 09:21:58 +0700

    Update coapi.php

--- coapi.php
+++ coapi.php
@@ -1,5 +1,13 @@
 <?php
- $json =  file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
- $data = json_decode($json);
- echo $data[0]->new_case;
+  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
+  $data = json_decode($json);
+  echo "ยอดผู้ป่วยวันนี้ : ".$data[0]->new_case."<br>";
+  echo "ยอดผู้ป่วยรวม : ".$data[0]->total_case."<br>";
+  echo "ยอดผู้ป่วยรวม : ".$data[0]->new_case_excludeabroad."<br>";
+  echo "ยอดผู้ป่วยรวม : ".$data[0]->total_case_excludeabroad."<br>";
+  echo "ยอดผู้ป่วยรวม : ".$data[0]->new_death."<br>";
+  echo "ยอดผู้ป่วยรวม : ".$data[0]->total_death."<br>";
+  echo "ยอดผู้ป่วยรวม : ".$data[0]->new_recovered."<br>";
+  echo "ยอดผู้ป่วยรวม : ".$data[0]->total_recovered."<br>";
+  echo "ยอดผู้ป่วยรวม : ".$data[0]->update_date."<br>";  
 ?>
