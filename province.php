b0e43af3007d2e3ac92e2d64356c04a9028a74ce
FolkXone <72967134+FolkXone@users.noreply.github.com>
2021-12-21 08:46:59 +0700

    Update province.php

--- province.php
+++ province.php
@@ -1,10 +1,12 @@
 <?php
 
-$json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
-$data = json_decode($json);
-
-foreach($data as $key=>$val){
-  echo $val->province.":".$val->new_case."<br>";
-}    
-
+  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
+  $data = json_decode($json);
+  
+  echo "<table border='1'>";
+  echo "<tr><td>ลำดับ</td><td>จังหวัด</td></tr>";
+  foreach($data as $key=>$val){
+    echo $val->province."<br>";
+  }
+  echo "</table>"; 
 ?>
