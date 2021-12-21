e715b00b1d7b1b9cca003b7849a0021a93d411d4
FolkXone <72967134+FolkXone@users.noreply.github.com>
2021-12-21 10:22:24 +0700

    Update province.php

--- province.php
+++ province.php
@@ -2,41 +2,54 @@
 
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
-echo "<h1><center><b>รายงานสถานการโควิด-19 แยกจังหวัด</b></center></h1>.'<br>'";
-echo '<body style="background-color:Turquoise">';
+
+    echo "<center><b>รายงานสถานการณ์โควิด-19 แยกจังหวัด</b></center>";
+    echo "<br><br>";
+
+echo '<body style="background-color:Pastel Green">';
 echo "<center><table border='1'>";
-echo "<tr><td>ลำดับ</td><td>จังหวัด</td><td>จำนวนผู้ป่วยใหม่</td><td>จำนวนผู้ป่วยสะสม</td>
-       <td>จำนวนผู้ป่วยใหม่(รวมต่างประเทศ)</td><td>จำนวนผู้ป่วยสะสม(รวมต่างประเทศ)</td>
-       <td>จำนวนผู้เสียชีวิต</td><td>จำนวนผู้เสียชีวิตสะสม</td></tr>";
+echo "<tr><td style= 'background-color:#FFCBA4'>ลำดับ</td>
+      <td style= 'background-color:#E8ADAA'>จังหวัด</td>
+      <td style= 'background-color:#E8ADAA'>จำนวนผู้ป่วยใหม่</td>
+      <td style= 'background-color:#E8ADAA'>จำนวนผู้ป่วยสะสม</td>
+      <td style= 'background-color:#E8ADAA'>จำนวนผู้ป่วยใหม่(รวมต่างประเทศ)</td>
+      <td style= 'background-color:#E8ADAA'>จำนวนผู้ป่วยสะสม(รวมต่างประเทศ)</td>
+      <td style= 'background-color:#E8ADAA'>จำนวนผู้เสียชีวิต</td>
+      <td style= 'background-color:#E8ADAA'>จำนวนผู้เสียชีวิตสะสม</td>
+      </tr>";
   
 foreach($data as $key=>$val){
   echo "<tr>";
-  echo "<td>";
+  echo "<td style= 'background-color:#FFCBA4'>";
   echo ($key+1);
   echo "</td>";
-  echo "<td>";
+  echo "<td style= 'background-color:#E8ADAA'>";
   echo $val->province;
   echo "</td>";
-  echo "<td>";
+  echo "<td style= 'background-color:#E8ADAA'>";
   echo $val->new_case;
   echo "</td>";
-  echo "<td>";
+  echo "<td style= 'background-color:#E8ADAA'>";
   echo $val->total_case;
   echo "</td>";
-  echo "<td>";
+  echo "<td style= 'background-color:#E8ADAA'>";
   echo $val->new_case_excludeabroad;
   echo "</td>";
-  echo "<td>";
+  echo "<td style= 'background-color:#E8ADAA'>";
   echo $val->total_case_excludeabroad;
   echo "</td>";
-  echo "<td>";
+  echo "<td style= 'background-color:#E8ADAA'>";
   echo $val->new_death;
   echo "</td>";
-  echo "<td>";
+  echo "<td style= 'background-color:#E8ADAA'>";
   echo $val->total_death;
   echo "</td>";
   echo "</tr>";
 }
 echo "</table></center>";
+
+echo "<br><br>";
+echo "วันที่อัพเดต";
+echo $val->txn_date;
 
 ?>
