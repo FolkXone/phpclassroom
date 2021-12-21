26a0bdcaeccdbca15553bd51c83587dd2c74b9e3
FolkXone <72967134+FolkXone@users.noreply.github.com>
2021-12-21 09:30:50 +0700

    Update province.php

--- province.php
+++ province.php
@@ -1,5 +1,6 @@
 <?php
-
+<html>
+<body bgcolor="#FFFFE6">
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
 
@@ -38,5 +39,6 @@ foreach($data as $key=>$val){
   echo "</tr>";
 }
 echo "</table>";
-
+</body>
+</html>
 ?>
