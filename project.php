4bbcf8b9e552ceb28a81c48c560f09cb5c33b61c
FolkXone <72967134+FolkXone@users.noreply.github.com>
2022-01-18 10:32:51 +0700

    Update project.php

--- project.php
+++ project.php
@@ -135,6 +135,7 @@
           </tr>
           <!-- แก้ไขส่วนที่ 3 -->
           <!-- Loop -->
+          <tr>
           <?php
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
